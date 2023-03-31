cc6d570eb48aef36a8ae0195fe2b52941fae8027
Doucouré Abdietou <a.doucoure@ymail.com>
2023-03-31 23:01:11 +0200

    Backend - build flask api for manage history data

---
+++ flask-api/README.md
@@ -0,0 +1,43 @@
+# flask-api
+
+## Requirements
+
+To run this application on your workstation, you must install :
+
+-  Python 3.10.xx
+
+## Create the virtual environment
+```bash
+$ python -m venv venv
+```
+
+## Activate the virtual environment
+### Windows
+```bash
+$ venv\Scripts\activate
+```
+
+Or run this command for cmd.exe
+```bash
+$ venv\Scripts\activate.bat
+```
+
+### Linux or macOS
+```bash
+$ source venv/bin/activate
+```
+
+## Install dependencies
+```bash
+$ pip install -r requirements.txt
+```
+
+## Start Server
+```bash
+$ flask run
+```
+
+Or run this command 
+```bash
+$ python -m flask run
+```
---
+++ flask-api/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+# create an instance of flask
+app = Flask(__name__)
+
+# creating an API object
+api = Api(app)
+
+# create database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///history.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#sqlalchemy mapper
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+class History(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"{self.content}"
+
+# For GET request to http://localhost:5000/
+class GetHistory(Resource):
+    def get(self):
+        history = History.query.all()
+        hist_list = []
+        for hist in history:
+            hist_data = {'Id': hist.id, 'Content': hist.content}
+            hist_list.append(hist_data)
+        return {"history": hist_list}, 200
+
+# For Post request to http://localhost:5000/add
+class AddHistory(Resource):
+    def post(self):
+        if request.is_json:
+            hist = History(content=request.json['Content'])
+            db.session.add(hist)
+            db.session.commit()
+            # return a json response
+            return make_response(jsonify({'Id': hist.id, 'Content': hist.content}), 201)
+        else:
+            return {'error': 'Request must be JSON'}, 400
+
+# For put request to http://localhost:5000/update/?
+class UpdateHistory(Resource):
+    def put(self, id):
+        if request.is_json:
+            hist = History.query.get(id)
+            if hist is None:
+                return {'error': f'History with id {id} not found'}, 404
+            else:
+                hist.content = request.json['Content']
+                db.session.commit()
+                return 'Updated', 200
+        else:
+            return {'error': 'Request must be JSON'}, 400
+
+# For delete request to http://localhost:5000/delete/?
+class DeleteHistory(Resource):
+    def delete(self, id):
+        hist = History.query.get(id)
+        if hist is None:
+            return {'error': f'History with id {id} not found'}, 404
+        db.session.delete(hist)
+        db.session.commit()
+        return f'History with id {id} is deleted', 200
+
+
+api.add_resource(GetHistory, '/')
+api.add_resource(AddHistory, '/add')
+api.add_resource(UpdateHistory, '/update/<int:id>')
+api.add_resource(DeleteHistory, '/delete/<int:id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
